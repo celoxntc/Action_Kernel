cdf236dec193bd5847c2b74928393fdf3ffd8839
celoxntc <98237249+celoxntc@users.noreply.github.com>
2023-10-19 22:38:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 heusecho "===================Setup Export========================="
 export ARCH=arm64
+export CLANG_TRIPLE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-gnu-
 export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
 
